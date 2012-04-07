c5f3d8a340841d5cfbd27777d7ec0214f57cda57
Niklas Lindström <lindstream@gmail.com>
2012-04-07 16:19:44 +0200

    Improve read-into-model and add RDFaReader state

--- src/rdfa/adapter/jena.clj
+++ src/rdfa/adapter/jena.clj
@@ -17,8 +17,10 @@
     BNode (.createResource model
                            (com.hp.hpl.jena.rdf.model.AnonId. (:id term)))))
 
-(defn read-into-model [model source base]
-  (let [{:keys [env triples proc-triples]} (rdfa.parser/get-rdfa source base)]
+(defn read-into-model [model & args]
+  (let [{:keys [env triples proc-triples]} (apply rdfa.parser/get-rdfa args)]
+    (.setNsPrefixes model (:prefix-map env))
+    ;(.setNsPrefix model (:vocab env))
     (doseq [[s p o] triples]
       (.add model
             (create-node model s)
@@ -36,22 +38,26 @@
 (gen-class
   :name rdfa.adapter.jena.RDFaReader
   :implements [com.hp.hpl.jena.rdf.model.RDFReader]
+  :state state
+  :init init
   :prefix "parser-")
 
+(defn parser-init []
+  [[] (atom {})])
+
 (defn parser-setErrorHandler [this err-handler]
-  err-handler)
+  (swap! (.state this) assoc :error-handler err-handler))
 
 (defn parser-setProperty [this pname value]
-  nil)
+  (swap! (.state this) assoc pname value))
 
 (defn parser-read
-  ;parser-read-Model-InputStream-String
-  ;parser-read-Model-Reader-String
+  ;parser-read-Model-(InputStream/Reader)-String
   ([this model source base]
    (read-into-model model source base))
   ;parser-read-Model-String
   ([this model url]
-   (read-into-model model url url)))
+   (read-into-model model url)))
 
 
 (gen-class
